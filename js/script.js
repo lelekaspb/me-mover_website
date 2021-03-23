132066d6d8d33089800c2ed519f2d9b63ac8d370
lelekaspb <bov-1986@yandex.ru>
2021-03-23 12:34:23 +0100

    based on feedback

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let mql = window.matchMedia('(max-width: 750px)');
 
-mql.addEventListener( "change", (e) => {
-    if (e.matches) {
+// mql.addEventListener( "change", (e) => {
+    if (mql.matches) {
     /* the viewport is 750 pixels wide or less */
     console.log('This is a narrow screen — less than 750px wide.')
     document.querySelector("#video-bg").removeAttribute('autoplay');
@@ -13,4 +13,4 @@ mql.addEventListener( "change", (e) => {
     /* the viewport is more than than 750 pixels wide */
     console.log('This is a wide screen — more than 750px wide.')
   }
-})
+// })
