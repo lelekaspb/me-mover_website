b797c7b696851318e5f5163897c8bf3cfa769c48
lelekaspb <bov-1986@yandex.ru>
2021-03-23 11:14:05 +0100

    added js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let mql = window.matchMedia('(max-width: 750px)');
+
+mql.addEventListener( "change", (e) => {
+    if (e.matches) {
+    /* the viewport is 750 pixels wide or less */
+    console.log('This is a narrow screen — less than 750px wide.')
+    document.querySelector("#video-bg").removeAttribute('autoplay');
+    document.querySelector("#video-bg").removeAttribute('muted');
+    document.querySelector("#video-bg").removeAttribute('loop');
+    console.log(document.querySelector("#video-bg"));
+    // document.querySelector("#video-bg").setAttributeNode('controls');
+  } else {
+    /* the viewport is more than than 750 pixels wide */
+    console.log('This is a wide screen — more than 750px wide.')
+  }
+})
