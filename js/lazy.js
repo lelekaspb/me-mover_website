91e6288bfea0e256283b94ab3eab3aade5c1ad6a
lelekaspb <bov-1986@yandex.ru>
2021-04-03 12:05:28 +0200

    added DOMContentLoaded to lazy.js

--- js/lazy.js
+++ js/lazy.js
@@ -1,30 +1,30 @@
 // lazy load the background video
+document.addEventListener("DOMContentLoaded", function () {
+  const lazyVideo = document.querySelector("video#video-bg");
 
-const lazyVideo = document.querySelector("video#video-bg");
+  const options = {
+    root: null,
+    threshold: 0,
+    rootMargin: "-50px",
+  };
 
-const options = {
-  root: null,
-  threshold: 0,
-  rootMargin: "-50px",
-};
-
-const observer = new IntersectionObserver(function (entries, observer) {
-  entries.forEach((video) => {
-    console.log(video.target.children);
-    if (video.isIntersecting) {
-      for (const source in video.target.children) {
-        const videoSource = video.target.children[source];
-        if (
-          typeof videoSource.tagName === "string" &&
-          videoSource.tagName === "SOURCE"
-        ) {
-          videoSource.src = videoSource.dataset.src;
+  const observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach((video) => {
+      console.log(video.target.children);
+      if (video.isIntersecting) {
+        for (const source in video.target.children) {
+          const videoSource = video.target.children[source];
+          if (
+            typeof videoSource.tagName === "string" &&
+            videoSource.tagName === "SOURCE"
+          ) {
+            videoSource.src = videoSource.dataset.src;
+          }
         }
+        video.target.load();
+        observer.unobserve(video.target);
       }
-      video.target.load();
-      observer.unobserve(video.target);
-    }
-  });
-}, options);
-
-observer.observe(lazyVideo);
+    });
+  }, options);
+  observer.observe(lazyVideo);
+});
